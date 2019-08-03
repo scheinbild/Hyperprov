b27506497827e5f31068d4d98377b9ed62d049ef
scheinbild <wns94@naver.com>
2019-08-03 00:56:03 -0700

    test1

--- init.sh
+++ init.sh
@@ -1,1 +1,1 @@
-docker swarm join --token SWMTKN-1-24rbfem7166qzu6wc678hryt3wtrxgyirsdupcjkpvwmmp5sal-74fo7wmobqbtqs2mssk23nhdt 192.168.23.132:2377
+docker swarm join --token SWMTKN-1-21rxuejr72yh1xyrosem2bg59ao28gpxwyckdm83m5zpzsvbst-e35wrzvytmd80i5q8jpdnswxx 192.168.23.132:2377
